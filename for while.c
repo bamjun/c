a60d65554f7e630e7cca705fbf2999a251a0bd3a
bamjun <khy5116@naver.com>
2021-03-19 13:57:21 +0900

    Update for while.c

--- for while.c
+++ for while.c
@@ -2,6 +2,29 @@
 
 int main()
 {
+  int a;
+  //a는 0부터 20보다 작으면 계속 a +1
+  // 0부터 19까지 출력
+  // for(초기; 조건; 증감)
+  for (a=0; a<20; a++)
+  {
+    printf("%d \n", a);
+  }
+  
+  a = 0;
+  for (;;)
+	{
+		scanf("%d", &a);
+		if (a == 5) {
+			printf("ok \n");
+			break;
+		}
+		else {
+			printf("no \n");
+		}
+	}
+  
+  
   
   
   return 0;
