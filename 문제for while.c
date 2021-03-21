5f0b251b851bb084f1a3c1e290185082e4cf9203
bamjun <khy5116@naver.com>
2021-03-21 19:36:11 +0900

    Update 문제for while.c

--- 문제for while.c
+++ 문제for while.c
@@ -127,3 +127,31 @@ int main()
 
 	return 0;
 }
+
+
+/*
+문제 5 (난이도 : 下)
+사용자로 부터 N 값을 입력 받고 1 부터 N 까지의 곱을 출력한다.
+*/
+#include <stdio.h>
+
+int main()
+{
+	int n, x, u = 1;
+	int i = 1;
+	printf("입력하세욥 :");
+	scanf("%d", &n);
+	for (x = 1; x <= n; x++)
+	{
+
+		u *= x;
+
+	}
+	printf("%d", u);
+	return 0;
+}
+
+
+
+
+	
