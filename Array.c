185c820f0327c4dadd4d32d03e8690a898386c9a
bamjun <khy5116@naver.com>
2021-03-25 09:41:45 +0900

    Update Array.c

--- Array.c
+++ Array.c
@@ -3,7 +3,9 @@
 int main() {
   int arr[10] = {2, 10, 30, 21, 34, 23, 53, 21, 9, 1};
   int i;
-  for (i = 0; i < 10; i++) {
+  printf("%d%d \n", sizeof(arr),sizeof(arr) / sizeof(int));
+  
+  for (i = 0; i < sizeof(arr) / sizeof(int); i++) {
     printf("배열의 %d 번째 원소 : %d \n", i + 1, arr[i]);
   }
   return 0;
